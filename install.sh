d0b3e9989ed833eda5e18c0b3ed22b95ec72f4d2
root <root@kenjiushiro>
2023-01-11 17:29:02 -0300

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 git config --global credential.helper store
+git config --global core.autocrlf true
 sudo apt install nodejs
 sudo apt install npm
+sudo apt install nvm
 sudo apt install python3
 sudo apt install zsh
+sudo apt install ripgrep
 sudo npm install -g typescript
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-git config --global core.autocrlf true
-git config --global credential.helper store
+curl -sLf https://raw.githubusercontent.com/kenjiushiro/nvim/main/utils/install.sh | sh
 
 cp ./.zshrc ~/.zshrc
 
