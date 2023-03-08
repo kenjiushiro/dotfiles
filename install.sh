fc8bba69bd0214dd8f4c89531efa4ee4d5518483
Kenji Martin Ushiro <kenjimartinushiro@gmail.com>
2023-03-08 10:35:37 -0300

    Rework dotfiles installation

--- install.sh
+++ install.sh
@@ -1,34 +1,7 @@
 git config --global credential.helper store
+# For WSL
 # git config --global credential.helper "/mnt/c/Users/username/AppData/Local/Programs/Git/mingw64/libexec/git-core/git-credential-wincred.exe"
-sudo apt --assume-yes update
-sudo apt --assume-yes install nodejs
-sudo apt --assume-yes install fzf
-sudo apt --assume-yes install npm
-sudo apt --assume-yes install python3
-sudo apt --assume-yes install zsh
-sudo apt --assume-yes install ripgrep
-sudo apt --assume-yes install golang
-sudo apt --assume-yes install cargo
-sudo apt --assume-yes install luarocks
-sudo apt --assume-yes install ruby
-sudo apt --assume-yes install composer
-sudo apt --assume-yes install php
-sudo apt --assume-yes install python3-pip
-sudo apt --assume-yes install python3.10-venv
-sudo npm --assume-yes install -g typescript
-sudo apt --assume-yes install fd-find
-sudo apt --assume-yes install ./nvim-linux64.deb
 
-pip install neovim
-sudo npm install -g neovim
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-curl -sLf https://raw.githubusercontent.com/kenjiushiro/nvim/main/utils/install.sh | sh
-
-./install_neovim.sh
-./install-tmux-plugin-manager.sh
-# ./install_alacritty.sh
-
-cp ./.zshrc ~/.zshrc
-cp ./alias.sh ~/alias.sh
+echo "export DOTFILES_PATH=$(pwd)" >> ~/.zshrc
+echo "source \$DOTFILES_PATH/.zshrc" >> ~/.zshrc
+echo 'ZSH_THEME="agnoster"' >> ~/.zshrc
---
+++ installers/essential_packages.sh
@@ -0,0 +1,25 @@
+sudo apt --assume-yes update
+sudo apt --assume-yes install nodejs
+sudo apt --assume-yes install fzf
+sudo apt --assume-yes install npm
+sudo apt --assume-yes install python3
+sudo apt --assume-yes install zsh
+sudo apt --assume-yes install ripgrep
+sudo apt --assume-yes install golang
+sudo apt --assume-yes install cargo
+sudo apt --assume-yes install luarocks
+sudo apt --assume-yes install ruby
+sudo apt --assume-yes install composer
+sudo apt --assume-yes install php
+sudo apt --assume-yes install python3-pip
+sudo apt --assume-yes install python3.10-venv
+sudo npm --assume-yes install -g typescript
+sudo apt --assume-yes install fd-find
+sudo apt --assume-yes install ./nvim-linux64.deb
+
+pip install neovim
+sudo npm install -g neovim
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -sLf https://raw.githubusercontent.com/kenjiushiro/nvim/main/utils/install.sh | sh
