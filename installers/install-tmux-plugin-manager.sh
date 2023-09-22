0d51a2e35a06d0e5ae94bda5997410b4cd648d06
kenjiushiro <kenjiushiro@kenjiushiro.penji.com>
2023-09-22 17:15:35 -0400

    Fix tmux installer

--- installers/install-tmux-plugin-manager.sh
+++ installers/install-tmux-plugin-manager.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm ~/.tmux.conf
-cp ./.tmux.conf ~/.tmux.conf
+cp ../configs/.tmux.conf ~/.tmux.conf
 tmux source ~/.tmux.conf
