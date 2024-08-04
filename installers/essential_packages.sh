900e4bc589d087a2571474a67cda398f37570bbe
Kenji Ushiro <kenjimartinushiro@gmail.com>
2024-08-04 02:58:43 -0300

    Create essential packages for arch

--- installers/essential_packages.sh
+++ installers/essential_packages.sh
@@ -1,38 +1,34 @@
-sudo apt --assume-yes update
-sudo apt --assume-yes install nodejs
-sudo apt --assume-yes install fzf
-sudo apt --assume-yes install npm
-sudo apt --assume-yes install python3
-sudo apt --assume-yes install zsh
-sudo apt --assume-yes install ripgrep
-sudo apt --assume-yes install golang
-sudo apt --assume-yes install cargo
-sudo apt --assume-yes install luarocks
-sudo apt --assume-yes install ruby
-sudo apt --assume-yes install composer
-sudo apt --assume-yes install php
-sudo apt --assume-yes install python3-pip
-sudo apt --assume-yes install python3.10-venv
+# sudo pacman --assume-yes update
+sudo pacman -S --noconfirm nodejs
+sudo pacman -S --noconfirm python3
+sudo pacman -S --noconfirm golang
+sudo pacman -S --noconfirm php
+
+sudo pacman -S --noconfirm fzf
+sudo pacman -S --noconfirm npm
+sudo pacman -S --noconfirm zsh
+sudo pacman -S --noconfirm ripgrep
+sudo pacman -S --noconfirm cargo
+sudo pacman -S --noconfirm luarocks
+sudo pacman -S --noconfirm ruby
+sudo pacman -S --noconfirm composer
+sudo pacman -S --noconfirm python3-pip
+sudo pacman -S --noconfirm python3.10-venv
 sudo npm --assume-yes install -g typescript
-sudo apt --assume-yes install fd-find
-sudo apt --assume-yes install tmux
-sudo apt --assume-yes install flameshot
-sudo apt --assume-yes install curl
-sudo apt --assume-yes install wmctrl
-python3 -m pip install --break-system-packages --user qmk
+sudo pacman -S --noconfirm fd-find
+sudo pacman -S --noconfirm tmux
+sudo pacman -S --noconfirm flameshot
+sudo pacman -S --noconfirm curl
+sudo pacman -S --noconfirm wmctrl
+sudo pacman -S --noconfirm neovim
+sudo pacman -S --noconfirm kitty
+sudo pacman -S --noconfirm fuse2
+
+sudo pacman -S --noconfirm pulseaudio
+sudo pacman -S --noconfirm kscreen
+
+sudo pacman -S --noconfirm lazygit
+sudo pacman -S --noconfirm lazydocker
 
 pip install --break-system-packages neovim
 sudo npm install -g neovim
-
-qmk setup
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-curl -sLf https://raw.githubusercontent.com/kenjiushiro/nvim/main/utils/install.sh | sh
-
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-
-go install github.com/jesseduffield/lazydocker@latest
