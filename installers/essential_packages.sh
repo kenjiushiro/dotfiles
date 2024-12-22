0ef89fb45727473cb1f3215cbf1ea4bb9ce520e1
Kenji Ushiro <kenjiushiro@Kenjis-MacBook-Pro.local>
2024-12-22 13:05:07 -0300

    Essential packages for brew

--- installers/essential_packages.sh
+++ installers/essential_packages.sh
@@ -1,38 +1,52 @@
-sudo apt --assume-yes update
-sudo apt --assume-yes install nodejs
-sudo apt --assume-yes install fzf
-sudo apt --assume-yes install npm
-sudo apt --assume-yes install python3
-sudo apt --assume-yes install zsh
-sudo apt --assume-yes install ripgrep
-sudo apt --assume-yes install golang
-sudo apt --assume-yes install cargo
-sudo apt --assume-yes install luarocks
-sudo apt --assume-yes install ruby
-sudo apt --assume-yes install composer
-sudo apt --assume-yes install php
-sudo apt --assume-yes install python3-pip
-sudo apt --assume-yes install python3.10-venv
-sudo npm --assume-yes install -g typescript
-sudo apt --assume-yes install fd-find
-sudo apt --assume-yes install tmux
-sudo apt --assume-yes install flameshot
-sudo apt --assume-yes install curl
-sudo apt --assume-yes install wmctrl
-python3 -m pip install --break-system-packages --user qmk
+#!/bin/bash
 
-pip install --break-system-packages neovim
+# Update Homebrew
+brew update
+
+# Install packages with Homebrew
+brew install node
+brew install fzf
+brew install npm
+brew install python3
+brew install zsh
+brew install ripgrep
+brew install go
+brew install cargo
+brew install luarocks
+brew install ruby
+brew install composer
+brew install php
+brew install python@3.10
+brew install fd
+brew install tmux
+brew install flameshot
+brew install curl
+
+# Optional: install wmctrl (not available on macOS; skip or replace with alternatives)
+# brew install wmctrl # Not supported on macOS
+
+# Install npm global packages
+sudo npm install -g typescript
 sudo npm install -g neovim
 
+# Install Python packages
+python3 -m pip install --user qmk
+python3 -m pip install --user neovim
+
+# QMK setup
 qmk setup
 
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+# Install custom Neovim configuration (ensure it's compatible with macOS)
 curl -sLf https://raw.githubusercontent.com/kenjiushiro/nvim/main/utils/install.sh | sh
 
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+# Install LazyGit
+brew install jesseduffield/lazygit/lazygit
 
-go install github.com/jesseduffield/lazydocker@latest
+# Install LazyDocker
+brew install jesseduffield/lazydocker/lazydocker
